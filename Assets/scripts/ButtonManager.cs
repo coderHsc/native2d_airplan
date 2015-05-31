c7975ce1a68323501b9b397305641ad5b4a9c679
w00w12l <hushencheng@coder.local>
2015-05-31 17:04:09 +0800

    add
    add

---
+++ Assets/scripts/ButtonManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonManager : MonoBehaviour {
+
+
+
+
+
+	public void onClick1(){
+		print("hello,world");
+		Debug.Log ("wocao !!!");
+	}
+}
---
+++ Assets/scripts/MenuScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	public void clickSingleGame(){
+
+		//Application.loadedLevel("GameScene");
+	}
+
+	public void clickNetGame(){
+
+		print (Application.loadedLevelName);
+		Application.LoadLevel("GameScene");
+	}
+}
